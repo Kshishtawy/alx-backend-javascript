a8c4c674442ab6d2b97ee01072b142f894c06b34
Kshishtawy <kareememadelshishtawy@gmail.com>
2024-03-10 05:29:12 +0000

    Fixing bugs

--- com.sh
+++ com.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	arg1="Fixing bugs"
+    arg1="Fixing bugs"
 else
-	arg1=$1
+    arg1="$1"
 fi
 
+echo "\nCommitting message: \"$arg1\"\n"
+
 git add .
-git commit -m $1
+git commit -m "$arg1"
 git push
+
+echo "\nChanges pushed to remote repository..."
